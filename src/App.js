9aa8e0552dc2cda3ebf0ce462ce7f764bfe5c4fb
Tan Dung Dong <dongtandung2001@gmail.com>
2022-10-15 19:20:18 -0700

    Add pagination and if we logically remove all movies from a page, it will go back to the previous page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 import MovieTable from './components/movies';
 
 function App() {
   return (
     <main className='container'>
+
       <MovieTable />
     </main>
   );
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import { deleteMovie } from "../services/fakeMovieService";
 import getMovies from "../services/fakeMovieService";
-import Like from "./like";
+import Like from "./common/like";
+import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
 
 class MovieTable extends Component {
   state = {
     movies: getMovies(),
+    pageSize: 4,
+    currentPage: 1,
   };
 
   removeFunc = (id) => {
@@ -21,71 +25,91 @@ class MovieTable extends Component {
     this.setState({ movies });
   };
 
+  handleChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
+    // If delete all movies on a page, don't show an empty page but rather go back to previus page
+    if (
+      this.state.currentPage >
+      Math.ceil(this.state.movies.length / this.state.pageSize)
+    ) {
+      this.setState({ currentPage: this.state.currentPage - 1 });
+    }
+
+    // Do not display table if there arent any movies in the database
+    if (this.state.movies.length === 0)
+      return <p>There are no movies in the database</p>;
+
+    const movies = paginate(
+      this.state.movies,
+      this.state.currentPage,
+      this.state.pageSize
+    );
     return (
       <React.Fragment>
-        {this.state.movies.length !== 0 && (
-          <h3>Showing {this.state.movies.length} movies in the database</h3>
-        )}
-        {this.state.movies.length === 0 && (
-          <h3>There are no movies in the database</h3>
-        )}
-        {this.state.movies && this.state.movies.length > 0 && (
-          <table className="table">
-            <thead className="thead-dark">
-              <tr>
-                <th scope="col">Title</th>
-                <th scope="col">Genre</th>
-                <th scope="col">Stock</th>
-                <th scope="col">Rate</th>
-                <th scope="col"></th>
-                <th scope="col"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.movies.map((movie) => {
-                const {
-                  _id,
-                  title,
-                  genre,
-                  numberInStock,
-                  dailyRentalRate,
-                  liked,
-                } = movie;
-                return (
-                  <tr key={_id}>
-                    <td className="w-10" key={title}>
-                      {title}
-                    </td>
-                    <td className="w-20" key={genre}>
-                      {genre.name}
-                    </td>
-                    <td className="w-20" key={numberInStock}>
-                      {numberInStock}
-                    </td>
-                    <td className="w-20" key={dailyRentalRate}>
-                      {dailyRentalRate}
-                    </td>
-                    <td className="w-15">
-                      <Like
-                        liked={liked}
-                        onLikeToggle={() => this.handleLike(movie)}
-                      />
-                    </td>
-                    <td className="w-15">
-                      <button
-                        onClick={() => this.removeFunc(_id)}
-                        className="btn btn-danger btn small"
-                      >
-                        Remove
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        )}
+        <h3>Showing {movies.length} movies in the database</h3>
+        <table className="table">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((movie) => {
+              const {
+                _id,
+                title,
+                genre,
+                numberInStock,
+                dailyRentalRate,
+                liked,
+              } = movie;
+              return (
+                <tr key={_id}>
+                  <td className="w-10" key={title}>
+                    {title}
+                  </td>
+                  <td className="w-20" key={genre}>
+                    {genre.name}
+                  </td>
+                  <td className="w-20" key={numberInStock}>
+                    {numberInStock}
+                  </td>
+                  <td className="w-20" key={dailyRentalRate}>
+                    {dailyRentalRate}
+                  </td>
+                  <td className="w-15">
+                    <Like
+                      liked={liked}
+                      onLikeToggle={() => this.handleLike(movie)}
+                    />
+                  </td>
+                  <td className="w-15">
+                    <button
+                      onClick={() => this.removeFunc(_id)}
+                      className="btn btn-danger btn small"
+                    >
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <Pagination
+          itemsCount={this.state.movies.length}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handleChange}
+          currentPage={this.state.currentPage}
+        />
       </React.Fragment>
     );
   }
